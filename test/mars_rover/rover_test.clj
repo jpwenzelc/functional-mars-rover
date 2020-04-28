b297eee1401299fc1189e6519b488b1a3e3cb0ea
devwenzel <jose.wenzel@codurance.com>
2020-04-28 08:40:25 -0400

    rename tests in rover-tests

--- test/mars_rover/rover_test.clj
+++ test/mars_rover/rover_test.clj
@@ -4,18 +4,17 @@
     [mars-rover.rover :refer :all]))
 
 (deftest rover-tests
-  (testing "that it has 0 0 :north as the default position"
-           (is (= {:position {:x 0 :y 0} :orientation :north} (execute))))
+  (testing "returns the rover position upon execution with an empty string command"
+           (is (= {:position {:x 0 :y 0} :orientation :north} (execute)))
+           (is (= {:position {:x 3 :y 5} :orientation :east} (execute 3 5 :east)))
+           (is (= {:position {:x 6 :y 2} :orientation :west} (execute 6 2 :west)))
+           (is (= {:position {:x 1 :y 2} :orientation :south} (execute 1 2 :south))))
 
-  (testing "that the position is returned"
-           (is (= {:position {:x 3 :y 5} :orientation :east} (execute 3 5 :east))))
-
-  (testing "that returns the position when the execute is the command"
+  (testing "returns the rover final position when execute the move string command"
            (is (= {:position {:x 0 :y 1} :orientation :north} (execute "M")))
            (is (= {:position {:x 0 :y 2} :orientation :north} (execute "MM")))
            (is (= {:position {:x 0 :y 6} :orientation :north} (execute "MMMMMM")))
            (is (= {:position {:x 3 :y 6} :orientation :north} (execute 3 0 :north "MMMMMM")))
            (is (= {:position {:x 1 :y 0} :orientation :east} (execute 0 0 :east "M")))
            (is (= {:position {:x -1 :y 0} :orientation :west} (execute 0 0 :west "M")))
-           (is (= {:position {:x 0 :y -1} :orientation :south} (execute 0 0 :south "M")))
-  ))
+           (is (= {:position {:x 0 :y -1} :orientation :south} (execute 0 0 :south "M")))))
